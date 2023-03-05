0df7996b843b2fc8acb2bdfbd03f60e320c4096c
Lee-DoHa <johnny550@naver.com>
2023-03-05 22:50:33 +0900

    Update MemberRepository.java

--- backend/src/main/java/com/kiwes/backend/member/repository/MemberRepository.java
+++ backend/src/main/java/com/kiwes/backend/member/repository/MemberRepository.java
@@ -3,10 +3,13 @@ package com.kiwes.backend.member.repository;
 import com.kiwes.backend.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByKakaoId(String kakaoId);
+    Optional<Member> findByKakaoId(Long kakaoId);
     Optional<Member> findByKakaoNickname(String kakaoNickname);
     Optional<Member> findByRefreshToken(String refreshToken);
+    Optional<Member> findByMemberToken(String memberToken);
+
 }
