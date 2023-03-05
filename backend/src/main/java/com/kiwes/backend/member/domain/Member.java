de156a9a6731d419ed5d3278f79f09d69229cd37
Lee-DoHa <johnny550@naver.com>
2023-03-05 20:25:25 +0900

    Update Member.java

--- backend/src/main/java/com/kiwes/backend/member/domain/Member.java
+++ backend/src/main/java/com/kiwes/backend/member/domain/Member.java
@@ -1,4 +1,62 @@
 package com.kiwes.backend.member.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    private Long kakaoId;
+
+    private String kakaoProfileImg;
+
+    private String kakaoEmail;
+
+    private String birthday;
+
+    private String gender;
+
+    private String nickname;
+
+    private Boolean privacyAgreement;
+
+    private Boolean termAgreement;
+
+    private String memberIntro;
+
+    @Column(length = 1000)
+    private String refreshToken;
+
+    @CreationTimestamp
+    private Timestamp createTime;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedTime;
+
+
+    public void updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public void addKakaoProfileImage(String kakaoProfileImg) {
+        this.kakaoProfileImg = kakaoProfileImg;
+    }
+
+
+
 }
