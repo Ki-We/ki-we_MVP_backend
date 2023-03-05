e467add5dfd9155a2a104082c55d14a40fda2289
Lee-DoHa <johnny550@naver.com>
2023-03-05 21:27:23 +0900

    Update SecurityConfig.java

--- backend/src/main/java/com/kiwes/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/kiwes/backend/global/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
+    public void configure(WebSecurity web) {
         web.httpFirewall(defaultHttpFirewall());
     }
 
