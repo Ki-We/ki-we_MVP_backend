6368824cf7de359dedadbeb0084c979b7b105746
Lee-DoHa <johnny550@naver.com>
2023-03-05 22:50:29 +0900

    Create MemberResponse.java

---
+++ backend/src/main/java/com/kiwes/backend/member/domain/MemberResponse.java
@@ -0,0 +1,24 @@
+package com.kiwes.backend.member.domain;
+
+import lombok.*;
+
+@Data
+@ToString
+@AllArgsConstructor
+@Builder
+public class MemberResponse {
+
+    private Long memberId;
+
+    private Long kakaoId;
+
+    private String profileImg;
+
+    private String nickname;
+
+    private String memberToken;
+
+    private String gender;
+
+    private String birth;
+}
