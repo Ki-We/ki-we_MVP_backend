fce22a77ee96abc092ebe8299e0139c1b19ab3e7
Lee-DoHa <johnny550@naver.com>
2023-03-05 21:08:34 +0900

    Create JwtRequestFilter.java

---
+++ backend/src/main/java/com/kiwes/backend/jwt/filter/JwtRequestFilter.java
@@ -0,0 +1,88 @@
+package com.kiwes.backend.jwt.filter;
+
+import com.kiwes.backend.jwt.service.JwtService;
+import com.kiwes.backend.member.domain.Member;
+import com.kiwes.backend.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtRequestFilter extends OncePerRequestFilter {
+    private final MemberRepository memberRepository;
+    private final JwtService jwtService;
+    private GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
+    private final String NO_CHECK_URL = "/oauth/token";
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (request.getRequestURI().equals(NO_CHECK_URL)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String refreshToken = jwtService
+                .extractRefreshToken(request)
+                .filter(jwtService::isTokenValid)
+                .orElse(null);
+
+        if (refreshToken != null) {
+            checkRefreshTokenAndReIssueAccessToken(response, refreshToken);
+            return;
+        }
+        checkAccessTokenAndAuthentication(request, response, filterChain);
+
+    }
+
+    private void checkAccessTokenAndAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        jwtService.extractAccessToken(request).filter(jwtService::isTokenValid).ifPresent(
+
+                accessToken -> jwtService.extractUsername(accessToken).ifPresent(
+
+                        kakaoNickname -> memberRepository.findByKakaoNickname(kakaoNickname).ifPresent(
+
+                                this::saveAuthentication
+                        )
+                )
+        );
+
+        filterChain.doFilter(request,response);
+    }
+
+    private void saveAuthentication(Member member) {
+        UserDetails user =  User.builder()
+                .username(member.getKakaoNickname())
+                .password(member.getRefreshToken())
+                .roles("USER")
+                .build();
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, authoritiesMapper.mapAuthorities(user.getAuthorities()));
+
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
+    }
+
+    private void checkRefreshTokenAndReIssueAccessToken(HttpServletResponse response, String refreshToken) {
+
+        memberRepository.findByRefreshToken(refreshToken).ifPresent(
+                member -> jwtService.sendAccessToken(response, jwtService.createAccessToken(member))
+        );
+
+    }
+}
