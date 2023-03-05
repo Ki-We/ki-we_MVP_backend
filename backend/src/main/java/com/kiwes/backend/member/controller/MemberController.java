393e5024e19061481abc8edb2e4eb521d5635788
Lee-DoHa <johnny550@naver.com>
2023-03-05 21:08:31 +0900

    Update MemberController.java

--- backend/src/main/java/com/kiwes/backend/member/controller/MemberController.java
+++ backend/src/main/java/com/kiwes/backend/member/controller/MemberController.java
@@ -1,4 +1,17 @@
 package com.kiwes.backend.member.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class MemberController {
+
+    @GetMapping("/oauth/token")
+    public ResponseEntity getLogin(@RequestParam("code") String code) {
+        return ResponseEntity.ok().body("");
+    }
 }
