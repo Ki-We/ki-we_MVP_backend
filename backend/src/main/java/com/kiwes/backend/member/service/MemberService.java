7a063fa3aca6e7dc68ec3ebca68740ddf43664db
Lee-DoHa <johnny550@naver.com>
2023-03-05 21:08:23 +0900

    Update MemberService.java

--- backend/src/main/java/com/kiwes/backend/member/service/MemberService.java
+++ backend/src/main/java/com/kiwes/backend/member/service/MemberService.java
@@ -1,4 +1,12 @@
 package com.kiwes.backend.member.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
+
+
 }
