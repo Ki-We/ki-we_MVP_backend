ed69f6daac6cc82180c2c5531f22417a1425bf8b
Lee-DoHa <johnny550@naver.com>
2023-03-14 23:04:03 +0900

    Update Qna.java

--- backend/src/main/java/com/kiwes/backend/qna/domain/Qna.java
+++ backend/src/main/java/com/kiwes/backend/qna/domain/Qna.java
@@ -1,4 +1,42 @@
 package com.kiwes.backend.qna.domain;
 
+import com.kiwes.backend.member.domain.Member;
+import com.kiwes.backend.post.domain.Post;
+import com.kiwes.backend.reply.domain.Reply;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
 public class Qna {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "qna_id")
+    private Long qnaId;
+
+    @CreationTimestamp
+    private Timestamp createTime;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedTime;
+
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member writer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @OneToOne(mappedBy = "qna", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Reply reply;
+
+    public void addReply(Reply reply) {
+        this.reply = reply;
+    }
 }
