4290f2709c9ac232f84862da2add2ab6fb2da6a8
Lee-DoHa <johnny550@naver.com>
2023-03-14 23:04:04 +0900

    Update Reply.java

--- backend/src/main/java/com/kiwes/backend/reply/domain/Reply.java
+++ backend/src/main/java/com/kiwes/backend/reply/domain/Reply.java
@@ -1,4 +1,38 @@
 package com.kiwes.backend.reply.domain;
 
+import com.kiwes.backend.member.domain.Member;
+import com.kiwes.backend.post.domain.Post;
+import com.kiwes.backend.qna.domain.Qna;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
 public class Reply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long replyId;
+
+    @CreationTimestamp
+    private Timestamp createTime;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedTime;
+
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member writer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @OneToOne
+    @JoinColumn(name = "qna_id")
+    private Qna qna;
 }
