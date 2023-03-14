1a96e9a06f39a9f58dc86494cbe6b0d971628dca
Lee-DoHa <johnny550@naver.com>
2023-03-14 23:03:50 +0900

    Update Comment.java

--- backend/src/main/java/com/kiwes/backend/comment/domain/Comment.java
+++ backend/src/main/java/com/kiwes/backend/comment/domain/Comment.java
@@ -1,4 +1,33 @@
 package com.kiwes.backend.comment.domain;
 
+import com.kiwes.backend.member.domain.Member;
+import com.kiwes.backend.post.domain.Post;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @CreationTimestamp
+    private Timestamp createTime;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedTime;
+
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member writer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
 }
