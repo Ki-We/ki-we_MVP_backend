75088c1e4a09c6d824e315f4958ea19d3413e8bd
Lee-DoHa <johnny550@naver.com>
2023-03-15 17:53:32 +0900

    Update PostController.java

--- backend/src/main/java/com/kiwes/backend/post/controller/PostController.java
+++ backend/src/main/java/com/kiwes/backend/post/controller/PostController.java
@@ -1,4 +1,29 @@
 package com.kiwes.backend.post.controller;
 
+import com.kiwes.backend.post.domain.PostCreate;
+import com.kiwes.backend.post.domain.PostResponse;
+import com.kiwes.backend.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping("/post")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@ModelAttribute PostCreate postCreate, @RequestPart(required = false)MultipartFile multipartFile) {
+        postService.savePost(postCreate, multipartFile);
+    }
+
+    @GetMapping("/post/{postId}")
+    @ResponseStatus(HttpStatus.OK)
+    public PostResponse getPost(@PathVariable Long postId) {
+
+    }
+
 }
