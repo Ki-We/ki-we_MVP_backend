b745f7c221dd0bac117bab4d852ec10d21bbebab
Lee-DoHa <johnny550@naver.com>
2023-03-17 01:51:26 +0900

    Update Comment.java

--- backend/src/main/java/com/kiwes/backend/comment/domain/Comment.java
+++ backend/src/main/java/com/kiwes/backend/comment/domain/Comment.java
@@ -2,6 +2,10 @@ package com.kiwes.backend.comment.domain;
 
 import com.kiwes.backend.member.domain.Member;
 import com.kiwes.backend.post.domain.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -9,6 +13,10 @@ import javax.persistence.*;
 import java.sql.Timestamp;
 
 @Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comment {
 
     @Id
