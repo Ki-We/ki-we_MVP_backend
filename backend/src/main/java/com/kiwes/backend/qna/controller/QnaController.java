c4b96dbf5b8a3703b5fce2262922f35629093b0f
Lee-DoHa <johnny550@naver.com>
2023-03-17 01:52:02 +0900

    Update QnaController.java

--- backend/src/main/java/com/kiwes/backend/qna/controller/QnaController.java
+++ backend/src/main/java/com/kiwes/backend/qna/controller/QnaController.java
@@ -1,4 +1,29 @@
 package com.kiwes.backend.qna.controller;
 
+import com.kiwes.backend.qna.domain.QnaCreate;
+import com.kiwes.backend.qna.domain.QnaResponse;
+import com.kiwes.backend.qna.service.QnaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class QnaController {
+
+    private final QnaService qnaService;
+
+    @PostMapping("/qna")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createQna(@ModelAttribute QnaCreate qnaCreate) {
+        qnaService.saveQna(qnaCreate);
+    }
+
+    @GetMapping("/qna/{postId}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<QnaResponse> getQna(@PathVariable Long postId) {
+        return qnaService.getQna(postId);
+    }
 }
