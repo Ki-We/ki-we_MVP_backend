288b8147e55a6c00e150fafe61fac837fbb4be24
Lee-DoHa <johnny550@naver.com>
2023-03-17 01:51:35 +0900

    Create Heart.java

---
+++ backend/src/main/java/com/kiwes/backend/heart/domain/Heart.java
@@ -0,0 +1,29 @@
+package com.kiwes.backend.heart.domain;
+
+import com.kiwes.backend.member.domain.Member;
+import com.kiwes.backend.post.domain.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Heart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long heartId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+
+}
