c0e9ffe0273d0109bca03295f195a64eb5aa2a3b
Lee-DoHa <johnny550@naver.com>
2023-03-17 01:51:29 +0900

    Create CommentResponse.java

---
+++ backend/src/main/java/com/kiwes/backend/comment/domain/CommentResponse.java
@@ -0,0 +1,20 @@
+package com.kiwes.backend.comment.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class CommentResponse {
+    private Long commentId;
+    private String body;
+    private String writerNickname;
+    private String writerProfileImg;
+    private String writerToken;
+    private Timestamp lastModifiedTime;
+    private Boolean isWriter;
+}
