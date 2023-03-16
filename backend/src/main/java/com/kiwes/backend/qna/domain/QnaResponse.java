eb1335178ff4cd78b5d18d7fb72248ca65de7b3b
Lee-DoHa <johnny550@naver.com>
2023-03-17 01:52:07 +0900

    Create QnaResponse.java

---
+++ backend/src/main/java/com/kiwes/backend/qna/domain/QnaResponse.java
@@ -0,0 +1,20 @@
+package com.kiwes.backend.qna.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class QnaResponse {
+    private Long qnaId;
+    private String body;
+    private Timestamp lastModifiedTime;
+    private String writerToken;
+    private String writerNickname;
+    private String writerProfileImg;
+    private Boolean isWriter;
+}
