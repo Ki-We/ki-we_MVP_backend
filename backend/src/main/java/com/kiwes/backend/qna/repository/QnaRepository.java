9dde3edc99627eb6a66e055d4506975077eeb06c
Lee-DoHa <johnny550@naver.com>
2023-03-17 20:53:50 +0900

    Update QnaRepository.java

--- backend/src/main/java/com/kiwes/backend/qna/repository/QnaRepository.java
+++ backend/src/main/java/com/kiwes/backend/qna/repository/QnaRepository.java
@@ -5,7 +5,9 @@ import com.kiwes.backend.qna.domain.Qna;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface QnaRepository extends JpaRepository<Qna, Long> {
+    Optional<Qna> findByPost(Post post);
     List<Qna> findAllByPost(Post post);
 }
