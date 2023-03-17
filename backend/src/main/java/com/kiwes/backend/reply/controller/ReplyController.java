62ae4b2f050a09a569086ee3970543a490cbf8b7
Lee-DoHa <johnny550@naver.com>
2023-03-17 20:53:53 +0900

    Update ReplyController.java

--- backend/src/main/java/com/kiwes/backend/reply/controller/ReplyController.java
+++ backend/src/main/java/com/kiwes/backend/reply/controller/ReplyController.java
@@ -1,4 +1,28 @@
 package com.kiwes.backend.reply.controller;
 
+import com.kiwes.backend.qna.domain.QnaCreate;
+import com.kiwes.backend.reply.domain.ReplyCreate;
+import com.kiwes.backend.reply.domain.ReplyResponse;
+import com.kiwes.backend.reply.service.ReplyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
 public class ReplyController {
+
+    private final ReplyService replyService;
+
+    @PostMapping("/reply")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createReply(@ModelAttribute ReplyCreate replyCreate) throws Exception {
+        replyService.saveReply(replyCreate);
+    }
+
+    @GetMapping("/reply/{qnaId}")
+    @ResponseStatus(HttpStatus.OK)
+    public ReplyResponse getReply(@PathVariable Long qnaId) {
+        return replyService.getReply(qnaId);
+    }
 }
