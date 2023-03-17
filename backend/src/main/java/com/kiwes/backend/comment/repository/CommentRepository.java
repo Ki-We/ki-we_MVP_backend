244c0c95a53707f339e46bf8e8366abb1406a880
Lee-DoHa <johnny550@naver.com>
2023-03-17 20:53:22 +0900

    Update CommentRepository.java

--- backend/src/main/java/com/kiwes/backend/comment/repository/CommentRepository.java
+++ backend/src/main/java/com/kiwes/backend/comment/repository/CommentRepository.java
@@ -5,8 +5,10 @@ import com.kiwes.backend.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    Optional<Comment> findByPost(Post post);
     List<Comment> findAllByPost(Post post);
 }
