0850b9b1dc86a7d862209d97fa731d00e93afc2c
Lee-DoHa <johnny550@naver.com>
2023-03-17 20:53:40 +0900

    Create Participation.java

---
+++ backend/src/main/java/com/kiwes/backend/participation/domain/Participation.java
@@ -0,0 +1,27 @@
+package com.kiwes.backend.participation.domain;
+
+import com.kiwes.backend.member.domain.Member;
+import com.kiwes.backend.post.domain.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Participation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long participationId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
