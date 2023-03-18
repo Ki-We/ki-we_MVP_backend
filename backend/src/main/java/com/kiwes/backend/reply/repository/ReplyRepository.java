2ae9caedb11926952790b1dd7f1cdbff6659fd36
Lee-DoHa <johnny550@naver.com>
2023-03-18 16:45:13 +0900

    Update ReplyRepository.java

--- backend/src/main/java/com/kiwes/backend/reply/repository/ReplyRepository.java
+++ backend/src/main/java/com/kiwes/backend/reply/repository/ReplyRepository.java
@@ -1,11 +1,14 @@
 package com.kiwes.backend.reply.repository;
 
+import com.kiwes.backend.member.domain.Member;
 import com.kiwes.backend.qna.domain.Qna;
 import com.kiwes.backend.reply.domain.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
     Optional<Reply> findByQna(Qna qna);
+    List<Reply> findAllByWriter(Member member);
 }
