6eb40f1179ac2cdd4b04f967b90971c8c36ba5df
Lee-DoHa <johnny550@naver.com>
2023-03-18 16:44:58 +0900

    Update CommentRepository.java

--- backend/src/main/java/com/kiwes/backend/comment/repository/CommentRepository.java
+++ backend/src/main/java/com/kiwes/backend/comment/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package com.kiwes.backend.comment.repository;
 
 import com.kiwes.backend.comment.domain.Comment;
+import com.kiwes.backend.member.domain.Member;
 import com.kiwes.backend.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,4 +12,5 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     Optional<Comment> findByPost(Post post);
     List<Comment> findAllByPost(Post post);
+    List<Comment> findAllByWriter(Member member);
 }
