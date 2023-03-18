fb3d94714982b045057e2eefe1205e5c84e49f4f
Lee-DoHa <johnny550@naver.com>
2023-03-18 15:33:49 +0900

    Update PostRepository.java

--- backend/src/main/java/com/kiwes/backend/post/repository/PostRepository.java
+++ backend/src/main/java/com/kiwes/backend/post/repository/PostRepository.java
@@ -1,8 +1,16 @@
 package com.kiwes.backend.post.repository;
 
+import com.kiwes.backend.member.domain.Member;
 import com.kiwes.backend.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    List<Post> findAllByHost(Member member);
+
+    @Query(value = "SELECT * FROM post ORDER BY view_count DESC limit 10", nativeQuery = true)
+    List<Post> recommendPost();
 }
