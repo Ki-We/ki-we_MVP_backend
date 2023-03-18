5e27797ffcc2c194952be48fd4ec228819af2acb
Lee-DoHa <johnny550@naver.com>
2023-03-18 15:33:50 +0900

    Update PostService.java

--- backend/src/main/java/com/kiwes/backend/post/service/PostService.java
+++ backend/src/main/java/com/kiwes/backend/post/service/PostService.java
@@ -7,9 +7,7 @@ import com.kiwes.backend.heart.repository.HeartRepository;
 import com.kiwes.backend.participation.repository.ParticipationRepository;
 import com.kiwes.backend.member.domain.Member;
 import com.kiwes.backend.member.repository.MemberRepository;
-import com.kiwes.backend.post.domain.Post;
-import com.kiwes.backend.post.domain.PostCreate;
-import com.kiwes.backend.post.domain.PostResponse;
+import com.kiwes.backend.post.domain.*;
 import com.kiwes.backend.post.repository.PostRepository;
 import com.kiwes.backend.qna.repository.QnaRepository;
 import lombok.RequiredArgsConstructor;
@@ -46,6 +44,7 @@ public class PostService {
                 .hashtag(postCreate.getHashtag())
                 .chatLink(postCreate.getChatLink())
                 .viewCount(0L)
+                .currentNum(1L)
                 .build();
 
         Member member = memberRepository.findByKakaoId(Long.parseLong(SecurityUtil.getLoginUsername())).orElseThrow();
@@ -61,7 +60,7 @@ public class PostService {
         Member loginMember = memberRepository.findByKakaoId(Long.parseLong(SecurityUtil.getLoginUsername())).orElseThrow();
         Post post = postRepository.findById(postId).orElseThrow();
         post.upperView();
-        PostResponse result = PostResponse.builder()
+        return PostResponse.builder()
                 .postId(post.getPostId())
                 .title(post.getTitle())
                 .body(post.getBody())
@@ -70,6 +69,7 @@ public class PostService {
                 .place(post.getPlace())
                 .price(post.getPrice())
                 .recruitNum(post.getRecruitNum())
+                .currentNum(post.getCurrentNum())
                 .meetingGender(post.getMeetingGender())
                 .language(post.getLanguage())
                 .category(post.getCategory())
@@ -83,7 +83,6 @@ public class PostService {
                 .hasComment(commentRepository.findByPost(post).isPresent())
                 .hasQna(qnaRepository.findByPost(post).isPresent())
                 .build();
-        return result;
     }
 
     public List<PostResponse> getPostList() {
@@ -101,6 +100,7 @@ public class PostService {
                             .place(post.getPlace())
                             .price(post.getPrice())
                             .recruitNum(post.getRecruitNum())
+                            .currentNum(post.getCurrentNum())
                             .meetingGender(post.getMeetingGender())
                             .language(post.getLanguage())
                             .category(post.getCategory())
@@ -118,5 +118,137 @@ public class PostService {
         });
 
         return result;
+    }
+
+    public List<PostResponse> getPostListMember(String memberToken) {
+        Member loginMember = memberRepository.findByKakaoId(Long.parseLong(SecurityUtil.getLoginUsername())).orElseThrow();
+        Member member = memberRepository.findByMemberToken(memberToken).orElseThrow();
+        List<Post> postList = postRepository.findAllByHost(member);
+        List<PostResponse> result = new ArrayList<>();
+        postList.forEach(post -> {
+            result.add(
+                    PostResponse.builder()
+                            .postId(post.getPostId())
+                            .title(post.getTitle())
+                            .body(post.getBody())
+                            .meetingDate(post.getMeetingDate())
+                            .deadLineDate(post.getDeadLineDate())
+                            .place(post.getPlace())
+                            .price(post.getPrice())
+                            .recruitNum(post.getRecruitNum())
+                            .currentNum(post.getCurrentNum())
+                            .meetingGender(post.getMeetingGender())
+                            .language(post.getLanguage())
+                            .category(post.getCategory())
+                            .hashtag(post.getHashtag())
+                            .chatLink(post.getChatLink())
+                            .hostId(post.getHost().getMemberId())
+                            .hostProfileImg(post.getHost().getProfileImg())
+                            .isHost(Objects.equals(loginMember.getMemberToken(), memberToken))
+                            .hasLike(heartRepository.findByMemberAndPost(loginMember, post).isPresent())
+                            .hasJoin(participationRepository.findByMemberAndPost(loginMember, post).isPresent())
+                            .hasComment(commentRepository.findByPost(post).isPresent())
+                            .hasQna(qnaRepository.findByPost(post).isPresent())
+                            .build()
+            );
+        });
+
+        return result;
+    }
+
+    public List<PostResponse> getRecommendPost() {
+        Member loginMember = memberRepository.findByKakaoId(Long.parseLong(SecurityUtil.getLoginUsername())).orElseThrow();
+        List<Post> postList = postRepository.recommendPost();
+        List<PostResponse> result = new ArrayList<>();
+        postList.forEach(post -> {
+            result.add(
+                    PostResponse.builder()
+                            .postId(post.getPostId())
+                            .title(post.getTitle())
+                            .body(post.getBody())
+                            .meetingDate(post.getMeetingDate())
+                            .deadLineDate(post.getDeadLineDate())
+                            .place(post.getPlace())
+                            .price(post.getPrice())
+                            .recruitNum(post.getRecruitNum())
+                            .currentNum(post.getCurrentNum())
+                            .meetingGender(post.getMeetingGender())
+                            .language(post.getLanguage())
+                            .category(post.getCategory())
+                            .hashtag(post.getHashtag())
+                            .chatLink(post.getChatLink())
+                            .hostId(post.getHost().getMemberId())
+                            .hostProfileImg(post.getHost().getProfileImg())
+                            .isHost(Objects.equals(loginMember.getMemberToken(), post.getHost().getMemberToken()))
+                            .hasLike(heartRepository.findByMemberAndPost(loginMember, post).isPresent())
+                            .hasJoin(participationRepository.findByMemberAndPost(loginMember, post).isPresent())
+                            .hasComment(commentRepository.findByPost(post).isPresent())
+                            .hasQna(qnaRepository.findByPost(post).isPresent())
+                            .build()
+            );
+        });
+
+        return result;
+    }
+
+    public void edit(Long postId, PostEdit postEdit, MultipartFile multipartFile) throws Exception {
+        Member loginMember = memberRepository.findByKakaoId(Long.parseLong(SecurityUtil.getLoginUsername())).orElseThrow();
+        Post post = postRepository.findById(postId).orElseThrow();
+        if(!Objects.equals(post.getHost().getMemberToken(), loginMember.getMemberToken())) {
+            throw new Exception();
+        }
+
+        PostEditor.PostEditorBuilder editorBuilder = post.toEditor();
+        if(postEdit.getTitle() != null) {
+            editorBuilder.title(postEdit.getTitle());
+        }
+        if(postEdit.getBody() != null) {
+            editorBuilder.body(postEdit.getBody());
+        }
+        if(postEdit.getMeetingDate() != null) {
+            editorBuilder.meetingDate(postEdit.getMeetingDate());
+        }
+        if(postEdit.getDeadLineDate() != null) {
+            editorBuilder.deadLineDate(postEdit.getDeadLineDate());
+        }
+        if(postEdit.getPlace() != null) {
+            editorBuilder.place(postEdit.getPlace());
+        }
+        if(postEdit.getPrice() != null) {
+            editorBuilder.price(postEdit.getPrice());
+        }
+        if(postEdit.getRecruitNum() != null) {
+            editorBuilder.recruitNum(postEdit.getRecruitNum());
+        }
+        if(postEdit.getMeetingGender() != null) {
+            editorBuilder.meetingGender(postEdit.getMeetingGender());
+        }
+        if(postEdit.getLanguage() != null) {
+            editorBuilder.language(postEdit.getLanguage());
+        }
+        if(postEdit.getCategory() != null) {
+            editorBuilder.category(postEdit.getCategory());
+        }
+        if(postEdit.getHashtag() != null) {
+            editorBuilder.hashtag(postEdit.getHashtag());
+        }
+        if(postEdit.getChatLink() != null) {
+            editorBuilder.chatLink(postEdit.getChatLink());
+        }
+
+        if(!multipartFile.isEmpty()) {
+            post.updatePostImage(s3Uploader.getThumbnailPath(s3Uploader.uploadImage(multipartFile)));
+        }
+
+        post.edit(editorBuilder.build());
+    }
+
+    public void deletePost(Long postId) throws Exception {
+        Member loginMember = memberRepository.findByKakaoId(Long.parseLong(SecurityUtil.getLoginUsername())).orElseThrow();
+        Post post = postRepository.findById(postId).orElseThrow();
+        if(!Objects.equals(post.getHost().getMemberToken(), loginMember.getMemberToken())) {
+            throw new Exception();
+        }
+        postRepository.delete(post);
     }
 }
